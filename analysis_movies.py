c2e30ce23835840a9076b6880d7eb07793797c1a
KoDaichi <chaikyud1126@gmail.com>
2025-12-29 20:04:14 +0900

    style:

--- analysis_movies.py
+++ analysis_movies.py
@@ -8,11 +8,12 @@ from weasyprint import HTML
 # ・評価の色付け
 # ・俳優名カウント：カウントが同じ場合に人が交互になってしまう、ランキングなっている名前を太字に
 
-#改行挿入関数
+
+# 改行挿入関数
 def wrap_text(text, width=16):
     if pd.isna(text):
         return ""
-    return '\n'.join(textwrap.wrap(str(text), width))
+    return "\n".join(textwrap.wrap(str(text), width))
 
 
 class Analysis_Movie:
@@ -21,92 +22,110 @@ class Analysis_Movie:
         # self.df["映画名"] = self.df["映画名"].apply(lambda x: wrap_text(x, width=16).replace('\n', '<br>'))
         # print(self.df)
 
-
-    def sort(self, target_rows : list):
+    def sort(self, target_rows: list):
         while target_rows:
             if target_rows[0] == "映画名":
                 self.df.sort_values(by="映画名", ascending=True, inplace=True)
             elif target_rows[0] == "ジャンル":
                 genre_order = [
-                    'アクション', 
-                    '西部劇', 
-                    'クライム', 
-                    'スリラー',
-                    'ミステリー', 
-                    'ミュージカル', 
-                    'ホラー', 
-                    '戦争', 
-                    '史実', 
-                    'ドラマ', 
-                    'ロマンス',
-                    'SF', 
-                    'ファンタジー',
-                    'コメディ', 
+                    "アクション",
+                    "西部劇",
+                    "クライム",
+                    "スリラー",
+                    "ミステリー",
+                    "ミュージカル",
+                    "ホラー",
+                    "戦争",
+                    "史実",
+                    "ドラマ",
+                    "ロマンス",
+                    "SF",
+                    "ファンタジー",
+                    "コメディ",
                 ]
-                self.df['ジャンル'] = pd.Categorical(self.df['ジャンル'], categories=genre_order, ordered=True)
-                self.df.sort_values(by='ジャンル', ascending=True, inplace=True)
+                self.df["ジャンル"] = pd.Categorical(
+                    self.df["ジャンル"], categories=genre_order, ordered=True
+                )
+                self.df.sort_values(by="ジャンル", ascending=True, inplace=True)
             elif target_rows[0] == "評価":
                 self.df.sort_values(by="評価", ascending=False, inplace=True)
             elif target_rows[0] == "年代":
                 self.df.sort_values(by="年代", ascending=True, inplace=True)
             elif target_rows[0] == "監督名":
-                director_counts = self.df['監督名'].value_counts()
-                self.df['tmp_cnt'] = self.df['監督名'].map(director_counts)
-                self.df.sort_values(by=['tmp_cnt', '監督名'], ascending=[False, True], inplace=True)
-                self.df.drop(columns='tmp_cnt', inplace=True)
+                director_counts = self.df["監督名"].value_counts()
+                self.df["tmp_cnt"] = self.df["監督名"].map(director_counts)
+                self.df.sort_values(
+                    by=["tmp_cnt", "監督名"], ascending=[False, True], inplace=True
+                )
+                self.df.drop(columns="tmp_cnt", inplace=True)
             elif target_rows[0] == "俳優名":
-                all_actors = self.df['俳優名'].dropna().str.split(',').explode().str.strip()
+                all_actors = (
+                    self.df["俳優名"].dropna().str.split(",").explode().str.strip()
+                )
                 actor_counts = all_actors.value_counts()
 
                 # 各行に含まれる俳優の最大頻度を割り当て
                 def get_max_count(actor_string):
                     if pd.isna(actor_string):
                         return 0
-                    actor_list = re.split(r'[,]', actor_string)
+                    actor_list = re.split(r"[,]", actor_string)
                     actor_list = [a.strip() for a in actor_list]
                     return max(actor_counts.get(actor, 0) for actor in actor_list)
 
-                self.df['tmp_cnt'] = self.df['俳優名'].apply(get_max_count)
-                self.df.sort_values(by=['tmp_cnt', '俳優名'], ascending=[False, True], inplace=True)
-                self.df.drop(columns='tmp_cnt', inplace=True)
+                self.df["tmp_cnt"] = self.df["俳優名"].apply(get_max_count)
+                self.df.sort_values(
+                    by=["tmp_cnt", "俳優名"], ascending=[False, True], inplace=True
+                )
+                self.df.drop(columns="tmp_cnt", inplace=True)
 
             target_rows.pop(0)
 
-
     def _highlight_genre(self, genre):
         color_map = {
-            'アクション': '#FF6347',   # tomato
-            '西部劇': '#FF8C00',       # darkorange
-            'クライム': '#4169E1',   # royalblue
-            'スリラー': '#228B22',   # forestgreen
-            'ミステリー': '#DDA0DD',     # plum
-            'ミュージカル': '#DDA0DD',     # plum
-            'ホラー': '#808080',       # grey
-            '戦争': '#D3D3D3',       # lightgrey
-            '史実': '#D3D3D3',       # lightgrey
-            'ドラマ': '#87CEFA',       # lightskyblue
-            'ロマンス': '#FFB6C1',         # lightpink
-            'SF' : '#90EE90',           # lightgreen
-            'ファンタジー' : '#90EE90',           # lightgreen
-            'コメディ': '#FFFF00',     # yellow
+            "アクション": "#FF6347",  # tomato
+            "西部劇": "#FF8C00",  # darkorange
+            "クライム": "#4169E1",  # royalblue
+            "スリラー": "#228B22",  # forestgreen
+            "ミステリー": "#DDA0DD",  # plum
+            "ミュージカル": "#DDA0DD",  # plum
+            "ホラー": "#808080",  # grey
+            "戦争": "#D3D3D3",  # lightgrey
+            "史実": "#D3D3D3",  # lightgrey
+            "ドラマ": "#87CEFA",  # lightskyblue
+            "ロマンス": "#FFB6C1",  # lightpink
+            "SF": "#90EE90",  # lightgreen
+            "ファンタジー": "#90EE90",  # lightgreen
+            "コメディ": "#FFFF00",  # yellow
         }
-        return f'background-color: {color_map.get(genre, "#FFFFFF")}'  # default = white
+        return f"background-color: {color_map.get(genre, '#FFFFFF')}"  # default = white
 
-    def export_pdf_with_genre_colors(self, output_filename='Movies_table.pdf'):
+    def export_pdf_with_genre_colors(self, output_filename="Movies_table.pdf"):
         # DataFrame config
-        styled = self.df.style \
-            .format({"評価": "{:.1f}"}) \
-            .applymap(self._highlight_genre, subset=['ジャンル']) \
-            .set_table_styles([
-                {'selector': 'th', 'props': [('font-size', '10pt'), 
-                                             ('font-weight', 'bold'), 
-                                             ("padding", "4px 8px")]},
-                {'selector': 'td', 'props': [('font-size', '8pt')]},
-                {"selector": ".row_heading", "props": [('font-size', '8pt'), 
-                                                       ("font-weight", "normal")]},
-            ]) \
-            .set_properties(subset=['ジャンル', '評価', '年代'], **{'text-align': 'center'})
-    
+        styled = (
+            self.df.style.format({"評価": "{:.1f}"})
+            .map(self._highlight_genre, subset=["ジャンル"])
+            .set_table_styles(
+                [
+                    {
+                        "selector": "th",
+                        "props": [
+                            ("font-size", "10pt"),
+                            ("font-weight", "bold"),
+                            ("padding", "4px 8px"),
+                        ],
+                    },
+                    {"selector": "td", "props": [("font-size", "8pt")]},
+                    {
+                        "selector": ".row_heading",
+                        "props": [("font-size", "8pt"), ("font-weight", "normal")],
+                    },
+                ]
+            )
+            .set_properties(
+                subset=["ジャンル", "評価", "年代"], **{"text-align": "center"}
+            )
+        )
+
         # DataFrame to HTML
         html = styled.to_html()
 
@@ -144,8 +163,7 @@ class Analysis_Movie:
 
 
 if __name__ == "__main__":
-    am = Analysis_Movie("movies.csv")  # 映画データのCSVファイル名
-    am.sort(['評価'])
+    am = Analysis_Movie("data/movies.csv")  # 映画データのCSVファイル名
+    am.sort(["評価"])
     # am.sort(['ジャンル'])
     am.export_pdf_with_genre_colors()
-
